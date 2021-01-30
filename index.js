312ad45f768158c6fdb7e9cad4630f919628b697
Andrew Liu <aliu5454@gmail.com>
2021-01-30 09:23:24 -0800

    Wireframe done somewhat

---
+++ index.js
@@ -0,0 +1,8 @@
+// DOM Queries
+let categorySubmit = document.querySelector(".product-category");
+
+// Category "Save" Button
+categorySubmit.addEventListener("click", e => {
+    e.preventDefault();
+    
+});
